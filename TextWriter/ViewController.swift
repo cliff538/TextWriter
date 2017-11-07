32c0696da1c1680ae85286c6e5cd03e1efe53d2d
cliff538 <cliff538@yahoo.com>
2017-11-06 22:14:44 -0700

    code and style at end of tutorial. Modify from here.

--- TextWriter/ViewController.swift
+++ TextWriter/ViewController.swift
@@ -15,10 +15,43 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		//makes keyboard come up on app launch or viewDidLoad. 
 		textView.becomeFirstResponder()
+		//allows notificatin of change in settings.
+		NotificationCenter.default.addObserver(self, selector: #selector(detectIfSettingsChanged), name: UserDefaults.didChangeNotification, object: nil)
+		
+		
+		
+		
 		}
 	
 	// MARK: - Outlets
 	@IBOutlet weak var textView: UITextView!
+	
+	// MARK: - Functions
+	@objc func detectIfSettingsChanged() {
+		
+		//testing: print("Night Mode Setting is: \(UserDefaults.standard.bool(forKey: "nightMode"))")
+		
+		//if night mode toggle is on
+		//apply night theme
+		// else restore default theme
+		if UserDefaults.standard.bool(forKey: "nightMode") == true {
+			//apply night theme
+			//see same code block below for shorter way. 
+			view.backgroundColor = UIColor.black
+			textView.textColor = UIColor.white
+			textView.keyboardAppearance = UIKeyboardAppearance.dark
+			UIApplication.shared.statusBarStyle = UIStatusBarStyle.lightContent
+			
+		} else {
+			//restore default theme
+			//see above...this is a cleaner/easier way to write the same code.
+			view.backgroundColor = .white
+			textView.textColor = .black
+			textView.keyboardAppearance = .default
+			UIApplication.shared.statusBarStyle = .default
+		}
+		
+	}
 	
 	
 	
